2c1c259069f4c2c748d036df164d79107d738340
nikhil-dev-395 <nikhilw395@gmail.com>
2024-10-21 01:34:20 +0530

    send file is completed

--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -12,15 +12,27 @@ router.use((req, res, next) => {
 });
 
 router.get("/", (req, res) => {
-  return res.render("index");
+  return res.render("index", {
+    title: "wolf share",
+  });
+});
+
+router.get("/send", (req, res) => {
+  return res.render("helpers/send", {
+    title: "send file",
+  });
 });
 
 router.get("/login", (req, res) => {
-  return res.render("auth/login");
+  return res.render("auth/login", {
+    title: "login",
+  });
 });
 
 router.get("/register", (req, res) => {
-  return res.render("auth/register");
+  return res.render("auth/register", {
+    title: "register",
+  });
 });
 
 router.get("/account", (req, res) => {
@@ -28,6 +40,7 @@ router.get("/account", (req, res) => {
   here i only add this values manually to show some data in ejs but add here mongodb db connection quickly to directly fetch the data form db
   */
   return res.render("account", {
+    title: "account",
     fileName: "hero",
     fileSize: 200,
     fileDownloadUrl: "https://code.visualstudio.com/docs/editor/editingevolved",
@@ -35,7 +48,9 @@ router.get("/account", (req, res) => {
 });
 
 router.get("*", (req, res) => {
-  return res.render("404/not-found");
+  return res.render("404/not-found", {
+    title: "404",
+  });
 });
 
 module.exports = { webRouter: router };
